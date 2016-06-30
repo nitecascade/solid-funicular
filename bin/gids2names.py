e095b6a10e98df68de096174c3216d2454209b42
Fil Machi <fil@macfellow.com>
2016-06-29 22:37:09 -0700

    Refactor bin/meetupdata.py, bin/gids2names.py

--- bin/gids2names.py
+++ bin/gids2names.py
@@ -3,60 +3,75 @@
 import click
 import json
 import sys
+from meetupdata import GroupGidMapper
 
-group_names_file = None
-group_names = None
-
-
-def load_groups_file(group_names_file):
-    global group_names
-    with open(group_names_file) as gfp:
-        group_names = dict()
-        for line in gfp:
-            line = line.strip()
-            gid, name = line.split(maxsplit=1)
-            group_names[gid] = name
-    print("read {} groups".format(len(group_names)))
-
-
-def generate_group_names(group_ids_file=None, group_ids_list=None,
-        group_ids_json=None, group_ids_str=None):
-    gid_set = set()
-    if group_ids_file:
-        with click.open_file(group_ids_file) as gfp:
-            for line in gfp:
-                for gid in line.split():
-                    gid_set.add(gid)
-    if group_ids_json:
-        for gid in json.loads(group_ids_list):
-            gid_set.add(str(gid))
-    if group_ids_list:
-        for gid in group_ids_list:
-            gid_set.add(gid)
-    if group_ids_str:
-        for gid in group_ids_str.split():
-            gid_set.add(gid)
-    for gid in sorted(gid_set):
-        yield gid, group_names[gid]
-
-
-@click.command()
-@click.option('--gids-file', 'group_ids_file', type=click.Path(),
-        help='File of group_ids.')
-@click.option('--gids-json', 'group_ids_json', help='group_ids, JSON string.')
-@click.option('--gids', 'group_ids_str', help='group_ids, space separated.')
-def go(group_ids_file, group_ids_json, group_ids_str):
-    print("group_ids_file: {!r}".format(group_ids_file))
-    print("group_ids_json: {!r}".format(group_ids_json))
-    print("group_ids_str: {!r}".format(group_ids_str))
-    all_names = generate_group_names(
-                    group_ids_file=group_ids_file,
-                    group_ids_str=group_ids_str,
-                    group_ids_json=group_ids_json,
-                    )
-    for gid, name in all_names:
-        print("{} {}".format(gid, name))
 
 if __name__ == '__main__':
-    load_groups_file("data/groups.txt")
+
+    def gather_group_ids(group_ids_file=None, group_ids_list=None,
+            group_ids_json=None, group_ids_str=None):
+        gid_set = set()
+        if group_ids_file:
+            with click.open_file(group_ids_file) as gfp:
+                for line in gfp:
+                    for gid in line.split():
+                        gid_set.add(gid)
+        if group_ids_json:
+            for gid in json.loads(group_ids_list):
+                gid_set.add(str(gid))
+        if group_ids_list:
+            for gid in group_ids_list:
+                gid_set.add(gid)
+        if group_ids_str:
+            for gid in group_ids_str.split():
+                gid_set.add(gid)
+        return gid_set
+
+
+    def group_values_gen(group_ids_file=None, group_ids_list=None,
+            group_ids_json=None, group_ids_str=None, gid_map=None, key=None):
+        if key is None:
+            key = "name"
+        gid_set = gather_group_ids(
+                        group_ids_file=group_ids_file,
+                        group_ids_list=group_ids_list,
+                        group_ids_json=group_ids_json,
+                        group_ids_str=group_ids_str,
+                        )
+        for gid in sorted(gid_set):
+            yield gid, gid_map.get(gid, key)
+
+    @click.command()
+    @click.option('--gids-file', 'group_ids_file', type=click.Path(),
+            help='File of group_ids.')
+    @click.option('--gids-json', 'group_ids_json',
+            help='group_ids, JSON string.')
+    @click.option('--gids', 'group_ids_str',
+            help='group_ids, space separated.')
+    @click.option('--key', 'key', default="name",
+            help='Field to return (name, members, description).')
+    @click.option('--groups-file', 'groups_file', type=click.Path(),
+            default="data/groups.txt",
+            help='File of group of group information (.json or .txt.')
+    def go(group_ids_file, group_ids_json, group_ids_str, key, groups_file):
+        print("group_ids_file: {!r}".format(group_ids_file))
+        print("group_ids_json: {!r}".format(group_ids_json))
+        print("group_ids_str: {!r}".format(group_ids_str))
+        print("groups_file: {!r}".format(groups_file))
+        #gid_map = GroupGidMapper("data/groups.txt")
+        #gid_map = GroupGidMapper("data/groups.json")
+        gid_map = GroupGidMapper(groups_file)
+        gid_map.load()
+        print("read {} group names from {!r}".format(
+            len(gid_map), gid_map._file))
+        all_values = group_values_gen(
+                        group_ids_file=group_ids_file,
+                        group_ids_str=group_ids_str,
+                        group_ids_json=group_ids_json,
+                        gid_map=gid_map,
+                        key=key,
+                        )
+        for gid, value in all_values:
+            print("{} {}".format(gid, value))
+
     go()
--- bin/meetupdata.py
+++ bin/meetupdata.py
@@ -5,6 +5,69 @@ import os
 from pprint import pformat
 import sys
 
+
+class GroupGidMapper:
+    def __init__(self, group_file):
+        self._file = group_file
+        self._group_gid_map = None
+        self._count = 0
+
+    def load(self):
+        ext = os.path.splitext(self._file)[1]
+        if ext == ".txt":
+            return self._load_txt()
+        if ext == ".json":
+            return self._load_json()
+
+    def _load_txt(self):
+        with open(self._file) as gfp:
+            self._group_gid_map = dict()
+            for line in gfp:
+                line = line.strip()
+                gid, name = line.split(maxsplit=1)
+                self._group_gid_map[gid] = dict(name=name)
+        self._count = len(self._group_gid_map)
+
+    def _load_json(self):
+        with open(self._file) as gfp:
+            self._group_gid_map = dict()
+            for line in gfp:
+                line = line.strip()
+                jobj = json.loads(line)
+                gid = str(jobj["id"])
+                name = jobj["name"]
+                members = int(jobj["members"])
+                description = jobj.get("description", "<none>")
+                self._group_gid_map[gid] = dict(
+                        name=name,
+                        members=members,
+                        description=description,
+                        )
+        self._count = len(self._group_gid_map)
+
+    def get(self, gid, key):
+        if self._group_gid_map is None:
+            self.load()
+        return self._group_gid_map[gid][key]
+
+    def __getitem__(self, gid):
+        if self._group_gid_map is None:
+            self.load()
+        return self._group_gid_map[gid]["name"]
+
+    def __setitem__(self, key, value):
+        raise RuntimeError("group_gid to group_name map is immutable!")
+
+    def __len__(self):
+        return self._count
+
+    def __iter__(self):
+        return iter(self._group_gid_map)
+
+    def __contains__(self, item):
+        return item in self.group_id_map
+
+
 def gen_group_id_and_file_path(stash_root):
     """
     The stash directory contains:
@@ -23,6 +86,7 @@ def gen_group_id_and_file_path(stash_root):
         for fname in file_list:
             group_id = os.path.splitext(fname)[0]
             yield group_id, os.path.join(dir_name, fname)
+
 
 def gen_group_id_and_member_ids(stash_root):
     """
@@ -44,6 +108,34 @@ def gen_group_id_and_member_ids(stash_root):
                 data = json.loads(line)
                 member_ids.add(str(data["member"]["id"]))
         yield group_id, member_ids
+
+
+def gen_reduced_data(stash_root):
+    """
+    The stash directory contains:
+        stash/
+            members_in_group_/
+                1/2/12345.json
+                1/2/12678.json
+                ...
+    This generator yields JSON objects:
+        {"gid": 12345, "gname": "a group", "mids": ["123", "2345", ...]}
+        {"gid": 12678, "gname": "another group", "mids": ["23", "345", ...]}
+    where mids is a list containing the group member_ids.
+    """
+    for gid, fpath in gen_group_id_and_file_path(stash_root):
+        member_ids = set()
+        with open(fpath) as fin:
+            for line in fin:
+                data = json.loads(line)
+                member_ids.add(str(data["member"]["id"]))
+        data = dict(
+                gid=gid,
+                group_name=group_name,
+                members=list(member_ids),
+                )
+        yield json.dumps(data)
+
 
 # A sample line from the json file for group_id 26434. The data downloaded
 # using the MeetUp API is the dict with key "member" in this structure. It
